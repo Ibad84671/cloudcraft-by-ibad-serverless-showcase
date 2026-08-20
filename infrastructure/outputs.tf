c2ffdd3309a878f533887a55aa71ed59ceed5481
Ibad Shaikh <ibad84671@gmail.com>
2026-08-20 13:04:31 +0530

    refactor: remove superseded Terraform outputs

--- infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_url" {
-  description = "CloudFront distribution URL"
-  value       = aws_cloudfront_distribution.this.domain_name
-}
-
-output "bucket_name" {
-  description = "S3 bucket name"
-  value       = aws_s3_bucket.website.id
-}
